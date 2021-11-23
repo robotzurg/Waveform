2a5da638ca25c37782ad6d1a87f58b60698da8f9
Jeffdev <robotzurg@gmail.com>
2021-11-23 11:45:21 -0700

    Update ping.js

--- discord-bot/commands/ping.js
+++ discord-bot/commands/ping.js
@@ -5,6 +5,6 @@ module.exports = {
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		interaction.editReply('Pong!');
+		interaction.editReply('Pong!!');
 	},
 };
