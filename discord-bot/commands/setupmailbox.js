51a750071a1dc9befb34c665b974ff792d13e32b
Jeffdev <robotzurg@gmail.com>
2022-10-10 16:51:52 -0600

    Hotfix
    **Hotfix**
    `Fixes:`
    - Potentially fixed a bug where `/setupmailbox` could not be run.
    - Fixed a bug where `/sendmail` would crash upon sending an album

--- discord-bot/commands/setupmailbox.js
+++ discord-bot/commands/setupmailbox.js
@@ -20,7 +20,7 @@ module.exports = {
         // Setup spotify web api stuff
         const spotifyApi = await spotify_api_setup(interaction.user.id);
         if (spotifyApi == false) return interaction.reply(`This command requires you to use \`/login\` `);
-        interaction.reply('Setting up mailbox...');
+        await interaction.reply('Setting up mailbox...');
 
         let playlist_name = interaction.options.getString('playlist_name');
         if (playlist_name == null) playlist_name = 'Waveform Mailbox';
@@ -51,17 +51,17 @@ module.exports = {
         }
 
         await spotifyApi.createPlaylist(playlist_name, { 'description': playlist_desc, 'public': true })
-        .then(function(data) {
+        .then(async function(data) {
             db.user_stats.set(interaction.user.id, data.body.id, 'mailbox_playlist_id');
             db.user_stats.set(interaction.user.id, channel.id, 'mailbox');
             db.server_settings.push(interaction.guild.id, [interaction.user.id, channel.id], 'mailboxes');
-            interaction.editReply(`Your mailbox has now been setup on Spotify, and \`/sendmail\` can now be used with it!\n` + 
+            await interaction.editReply(`Your mailbox has now been setup on Spotify, and \`/sendmail\` can now be used with it!\n` + 
             `If you need to delete the playlist for whatever reason, make sure you run this command again to setup a new one!\n\n` + 
             `You should also have a new channel created under your name in the "Mailboxes" category, this is where all of your mailbox happenings will be in!\n` + 
             `You can use this chat to review your mailbox tracks, and it will also have messages sent in it any time you receive new mail.\n\n` +
             `**NOTE: DO NOT DELETE THIS PLAYLIST OR THE MAILBOX TEXT CHANNEL, OR ELSE YOUR MAILBOX WILL NOT WORK PROPERLY!**`);
-        }, function(err) {
-            interaction.editReply(`Something went wrong with your mailbox creation, you should probably let Jeff know!`);
+        }, async function(err) {
+            await interaction.editReply(`Something went wrong with your mailbox creation, you should probably let Jeff know!`);
             console.log('Something went wrong!', err);
         });
 
